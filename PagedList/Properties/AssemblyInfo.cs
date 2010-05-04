c72ffe0468aaa01800e23aede1912d003777834b
Troy Goode <troygoode@gmail.com>
2010-05-03 23:29:12 -0400

    Upgraded solution to VS2010 and set assembly attribute for partial trust.

--- PagedList/Properties/AssemblyInfo.cs
+++ PagedList/Properties/AssemblyInfo.cs
@@ -1,5 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: AllowPartiallyTrustedCallers]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
