a9a46415e4df4e630dc8b2d84f84d74aaa1e1253
Troy Goode <troygoode@gmail.com>
2010-10-06 13:17:10 -0400

    Added rake dependency chain.

--- rakefile.rb
+++ rakefile.rb
@@ -9,12 +9,12 @@ msbuild :build do |msb|
   msb.solution = "src/PagedList.sln"
 end
 
-xunit :test do |xunit|
+xunit :test => :build do |xunit|
   xunit.path_to_command = "src/PagedList.Tests/bin/debug/xunit.console.exe"
   xunit.assembly = "src/PagedList.Tests/bin/debug/PagedList.Tests.dll"
 end
 
-msbuild :release do |msb|
+msbuild :release => :test do |msb|
   msb.path_to_command =  File.join(ENV['windir'], 'Microsoft.NET', 'Framework',  'v4.0.30319', 'MSBuild.exe')
   msb.properties :configuration => :Release
   msb.targets :Clean, :Rebuild
