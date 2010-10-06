4834b784e9f37b04aeec4e796ff19610c5fdab7c
Troy Goode <troygoode@gmail.com>
2010-10-06 16:06:05 -0400

    Updated readme examples.

--- src/PagedList.Tests/PagedListExample.cs
+++ src/PagedList.Tests/PagedListExample.cs
@@ -1,6 +1,4 @@
-# Using PagedList<T>
-<pre>
-using System;
+﻿using System;
 using System.Linq;
 
 namespace PagedList.Tests
@@ -34,40 +32,3 @@ namespace PagedList.Tests
 		}
 	}
 }
-</pre>
-
-# Using StaticPagedList<T>
-<pre>
-using System;
-using System.Linq;
-
-namespace PagedList.Tests
-{
-	public class StaticPagedListExample
-	{
-		public static void Main(string[] args)
-		{
-			// create a list of integers from 1 to 20
-			var list = Enumerable.Range(1, 20);
-
-			var firstPage = new StaticPagedList&lt;int&gt;(list, 0, 20, 100); // first page
-			Console.WriteLine("Is first page? {0}", firstPage.IsFirstPage); // true
-			Console.WriteLine("Is last page? {0}", firstPage.IsLastPage); // false
-			Console.WriteLine("First value on page? {0}", firstPage[0]); // 1
-			Console.WriteLine();
-
-			var middlePage = new StaticPagedList&lt;int&gt;(list, 2, 20, 100); // third page, same values
-			Console.WriteLine("Is first page? {0}", middlePage.IsFirstPage); // false
-			Console.WriteLine("Is last page? {0}", middlePage.IsLastPage); // false
-			Console.WriteLine("First value on page? {0}", middlePage[0]); // 1
-			Console.WriteLine();
-
-			var lastPage = new StaticPagedList&lt;int&gt;(list, 4, 20, 100); // fifth page, same values
-			Console.WriteLine("Is first page? {0}", lastPage.IsFirstPage); // false
-			Console.WriteLine("Is last page? {0}", lastPage.IsLastPage); // true
-			Console.WriteLine("First value on page? {0}", lastPage[0]); // 1
-			Console.WriteLine();
-		}
-	}
-}
-</pre>
---
+++ src/PagedList.Tests/StaticPagedListExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace PagedList.Tests
+{
+	public class StaticPagedListExample
+	{
+		public static void Main(string[] args)
+		{
+			// create a list of integers from 1 to 20
+			var list = Enumerable.Range(1, 20);
+
+			var firstPage = new StaticPagedList<int>(list, 0, 20, 100); // first page
+			Console.WriteLine("Is first page? {0}", firstPage.IsFirstPage); // true
+			Console.WriteLine("Is last page? {0}", firstPage.IsLastPage); // false
+			Console.WriteLine("First value on page? {0}", firstPage[0]); // 1
+			Console.WriteLine();
+
+			var middlePage = new StaticPagedList<int>(list, 2, 20, 100); // third page, same values
+			Console.WriteLine("Is first page? {0}", middlePage.IsFirstPage); // false
+			Console.WriteLine("Is last page? {0}", middlePage.IsLastPage); // false
+			Console.WriteLine("First value on page? {0}", middlePage[0]); // 1
+			Console.WriteLine();
+
+			var lastPage = new StaticPagedList<int>(list, 4, 20, 100); // fifth page, same values
+			Console.WriteLine("Is first page? {0}", lastPage.IsFirstPage); // false
+			Console.WriteLine("Is last page? {0}", lastPage.IsLastPage); // true
+			Console.WriteLine("First value on page? {0}", lastPage[0]); // 1
+			Console.WriteLine();
+		}
+	}
+}
