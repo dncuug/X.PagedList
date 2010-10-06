60c4cbe2e8da6102b509ea08a8fdfda5800e452c
Troy Goode <troygoode@gmail.com>
2010-10-06 13:10:47 -0400

    Replaced Test.bat with Albacore/Rake task.

--- Test.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF 
-src\PagedList.Tests\bin\debug\xunit.console src\PagedList.Tests\bin\debug\PagedList.Tests.dll
-PAUSE
--- rakefile.rb
+++ rakefile.rb
@@ -2,10 +2,14 @@ require 'albacore'
 
 task :default => [:build]
 
-desc "Run a sample build using the MSBuildTask"
 msbuild :build do |msb|
   msb.path_to_command =  File.join(ENV['windir'], 'Microsoft.NET', 'Framework',  'v4.0.30319', 'MSBuild.exe')
   msb.properties :configuration => :Debug
   msb.targets :Clean, :Rebuild
   msb.solution = "src/PagedList.sln"
+end
+
+xunit :test do |xunit|
+  xunit.path_to_command = "src/PagedList.Tests/bin/debug/xunit.console.exe"
+  xunit.assembly = "src/PagedList.Tests/bin/debug/PagedList.Tests.dll"
 end
