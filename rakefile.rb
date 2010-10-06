1d9cc065116bb50f69efac4b834f23b958f9c3d8
Troy Goode <troygoode@gmail.com>
2010-10-06 13:40:01 -0400

    Added NuPack package description and rake task to generate package contents.

--- rakefile.rb
+++ rakefile.rb
@@ -20,3 +20,12 @@ msbuild :release => :test do |msb|
   msb.targets :Clean, :Rebuild
   msb.solution = "src/PagedList.sln"
 end
+
+task :package => :release do
+  require 'fileutils'
+  build_directory = './src/PagedList/bin/Release/'
+  output_directory = './packages/PagedList/lib/40/'
+  FileUtils.cp build_directory + 'PagedList.dll', output_directory + 'PagedList.dll'
+  FileUtils.cp build_directory + 'PagedList.pdb', output_directory + 'PagedList.pdb'
+  FileUtils.cp build_directory + 'PagedList.xml', output_directory + 'PagedList.xml'
+end
