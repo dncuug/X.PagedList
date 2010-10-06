357a5c9928ed9fef94b05ef9982632719936b821
Troy Goode <troygoode@gmail.com>
2010-10-06 13:13:40 -0400

    Replaced Release.bat with Albacore/Rake task.

--- rakefile.rb
+++ rakefile.rb
@@ -13,3 +13,10 @@ xunit :test do |xunit|
   xunit.path_to_command = "src/PagedList.Tests/bin/debug/xunit.console.exe"
   xunit.assembly = "src/PagedList.Tests/bin/debug/PagedList.Tests.dll"
 end
+
+msbuild :release do |msb|
+  msb.path_to_command =  File.join(ENV['windir'], 'Microsoft.NET', 'Framework',  'v4.0.30319', 'MSBuild.exe')
+  msb.properties :configuration => :Release
+  msb.targets :Clean, :Rebuild
+  msb.solution = "src/PagedList.sln"
+end
