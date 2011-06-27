11fdb988830879460fd21e012b5c96a11c8b799a
Troy Goode <troygoode@gmail.com>
2011-06-27 00:00:19 -0400

    Changed sample data in MVC Example app from numbers (which was slightly confusing) to a list of names.

--- src/PagedList.Mvc.Example/Controllers/HomeController.cs
+++ src/PagedList.Mvc.Example/Controllers/HomeController.cs
@@ -1,28 +1,39 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using System.IO;
 
 namespace PagedList.Mvc.Example.Controllers
 {
 	public class HomeController : Controller
 	{
-		public object Index(int? page)
+		public ActionResult Index(int? page)
 		{
 			// return a 404 if user browses to before the first page
 			if (page.HasValue && page < 1)
 				return HttpNotFound();
 
-			// retrieve list from database/wherever (in this case Enumerable.Range) and page it
+			// retrieve list from database/whereverand
+			var listUnpaged = GetStuffFromDatabase();
+
+			// page the list
 			const int pageSize = 20;
-			var listAll = Enumerable.Range(1, 500);
-			var listPaged = listAll.ToPagedList(page.HasValue ? page.Value - 1 : 0, pageSize);
+			var listPaged = listUnpaged.ToPagedList(page.HasValue ? page.Value - 1 : 0, pageSize);
 
 			// return a 404 if user browses to pages beyond last page. special case first page if no items exist
 			if (listPaged.PageNumber != 1 && page.HasValue && page > listPaged.PageCount)
 				return HttpNotFound();
 
 			// pass the paged list to the view and render
-			ViewBag.Numbers = listPaged;
+			ViewBag.Names = listPaged;
 			return View();
+		}
+
+		// in this case we return IEnumerable<int>, but in most
+		// - DB situations you'll want to return IQueryable<T>
+		private IEnumerable<string> GetStuffFromDatabase()
+		{
+			var sampleData = new StreamReader(Server.MapPath("~/SampleData.txt")).ReadToEnd();
+			return sampleData.Split('\n');
 		}
 	}
 }
