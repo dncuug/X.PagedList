3d22cc937ca4904279f0191884658a37ddeb53e2
Troy Goode <troygoode@gmail.com>
2011-06-27 20:52:21 -0400

    bumped revs up again. think I'm done tweaking for a while

--- src/PagedList.Mvc/Properties/AssemblyInfo.cs
+++ src/PagedList.Mvc/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Security;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.0.4.0")]
-[assembly: AssemblyFileVersion("3.0.4.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
--- src/PagedList/Properties/AssemblyInfo.cs
+++ src/PagedList/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Security;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.4.0")]
-[assembly: AssemblyFileVersion("1.2.4.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
