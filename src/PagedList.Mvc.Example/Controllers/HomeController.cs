1f1ccb0c3e16f47150f1f9ec094330bce2b6086b
Troy Goode <troygoode@gmail.com>
2011-07-06 00:45:39 -0400

    added working example of Ajax-based paging

--- src/PagedList.Mvc.Example/Controllers/HomeController.cs
+++ src/PagedList.Mvc.Example/Controllers/HomeController.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections.Generic;
-using System.Web.Mvc;
 using System.IO;
+using System.Web.Mvc;
 
 namespace PagedList.Mvc.Example.Controllers
 {
 	public class HomeController : Controller
 	{
+		// Traditional Paging
 		public ActionResult Index(int? page)
+		{
+			var listPaged = GetPagedNames(page);
+			if (listPaged == null)
+				return HttpNotFound();
+
+			// pass the paged list to the view and render
+			ViewBag.Names = listPaged;
+			return View();
+		}
+
+		// Ajax Paging
+		public ViewResult Ajax()
+		{
+			return View();
+		}
+
+		// Ajax Paging (cont'd)
+		public ActionResult AjaxPage(int? page)
+		{
+			var listPaged = GetPagedNames(page);
+			if (listPaged == null)
+				return HttpNotFound();
+
+			return Json(new
+			            	{
+			            		names = listPaged,
+			            		pager = new Pager(listPaged)
+			            	}, JsonRequestBehavior.AllowGet);
+		}
+
+		private IPagedList<string> GetPagedNames(int? page)
 		{
 			// return a 404 if user browses to before the first page
 			if (page.HasValue && page < 1)
-				return HttpNotFound();
+				return null;
 
 			// retrieve list from database/whereverand
 			var listUnpaged = GetStuffFromDatabase();
@@ -21,11 +53,9 @@ namespace PagedList.Mvc.Example.Controllers
 
 			// return a 404 if user browses to pages beyond last page. special case first page if no items exist
 			if (listPaged.PageNumber != 1 && page.HasValue && page > listPaged.PageCount)
-				return HttpNotFound();
+				return null;
 
-			// pass the paged list to the view and render
-			ViewBag.Names = listPaged;
-			return View();
+			return listPaged;
 		}
 
 		// in this case we return IEnumerable<int>, but in most
@@ -35,5 +65,43 @@ namespace PagedList.Mvc.Example.Controllers
 			var sampleData = new StreamReader(Server.MapPath("~/SampleData.txt")).ReadToEnd();
 			return sampleData.Split('\n');
 		}
+
+		#region Nested type: Pager
+
+		public class Pager : IPagedList
+		{
+			public Pager(IPagedList pagedList)
+			{
+				PageCount = pagedList.PageCount;
+				TotalItemCount = pagedList.TotalItemCount;
+				PageIndex = pagedList.PageIndex;
+				PageNumber = pagedList.PageNumber;
+				PageSize = pagedList.PageSize;
+				HasPreviousPage = pagedList.HasPreviousPage;
+				HasNextPage = pagedList.HasNextPage;
+				IsFirstPage = pagedList.IsFirstPage;
+				IsLastPage = pagedList.IsLastPage;
+				FirstItemOnPage = pagedList.FirstItemOnPage;
+				LastItemOnPage = pagedList.LastItemOnPage;
+			}
+
+			#region IPagedList Members
+
+			public int PageCount { get; private set; }
+			public int TotalItemCount { get; private set; }
+			public int PageIndex { get; private set; }
+			public int PageNumber { get; private set; }
+			public int PageSize { get; private set; }
+			public bool HasPreviousPage { get; private set; }
+			public bool HasNextPage { get; private set; }
+			public bool IsFirstPage { get; private set; }
+			public bool IsLastPage { get; private set; }
+			public int FirstItemOnPage { get; private set; }
+			public int LastItemOnPage { get; private set; }
+
+			#endregion
+		}
+
+		#endregion
 	}
 }
