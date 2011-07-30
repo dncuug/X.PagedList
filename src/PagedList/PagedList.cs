d4780f2c88292f347deb7a36a76bb9d9ad6f6e9b
Troy Goode <troygoode@gmail.com>
2011-07-30 09:03:31 -0400

    fixed issue #9

--- src/PagedList/PagedList.cs
+++ src/PagedList/PagedList.cs
@@ -25,15 +25,30 @@ namespace PagedList
 		/// <param name="pageSize">The maximum size of any individual subset.</param>
 		/// <exception cref="ArgumentOutOfRangeException">The specified index cannot be less than zero.</exception>
 		/// <exception cref="ArgumentOutOfRangeException">The specified page size cannot be less than one.</exception>
-		public PagedList(IEnumerable<T> superset, int index, int pageSize)
-			: this(superset == null ? new List<T>().AsQueryable() : superset.AsQueryable(), index, pageSize)
-		{
-		}
-
-		private PagedList(IQueryable<T> superset, int index, int pageSize) : base(index, pageSize, superset.Count())
+		public PagedList(IQueryable<T> superset, int index, int pageSize)
+			: base(index, pageSize, superset == null ? 0 : superset.Count())
 		{
 			// add items to internal list
-			if (TotalItemCount > 0)
+			if (superset != null && TotalItemCount > 0)
+				Subset.AddRange(index == 0
+					? superset.Skip(0).Take(pageSize).ToList()
+					: superset.Skip((index) * pageSize).Take(pageSize).ToList()
+				);
+		}
+		
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PagedList{T}"/> class that divides the supplied superset into subsets the size of the supplied pageSize. The instance then only containes the objects contained in the subset specified by index.
+		/// </summary>
+		/// <param name="superset">The collection of objects to be divided into subsets. If the collection implements <see cref="IQueryable{T}"/>, it will be treated as such.</param>
+		/// <param name="index">The index of the subset of objects to be contained by this instance.</param>
+		/// <param name="pageSize">The maximum size of any individual subset.</param>
+		/// <exception cref="ArgumentOutOfRangeException">The specified index cannot be less than zero.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">The specified page size cannot be less than one.</exception>
+		public PagedList(IEnumerable<T> superset, int index, int pageSize)
+			: base(index, pageSize, superset == null ? 0 : superset.Count())
+		{
+			// add items to internal list
+			if(superset != null && TotalItemCount > 0)
 				Subset.AddRange(index == 0
 					? superset.Skip(0).Take(pageSize).ToList()
 					: superset.Skip((index)*pageSize).Take(pageSize).ToList()
