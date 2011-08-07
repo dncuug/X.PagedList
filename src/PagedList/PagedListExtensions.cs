f4a55f4dd2c8f29280a1dc9a200ae2c2fc714065
Troy Goode <troygoode@gmail.com>
2011-08-07 09:18:58 -0400

    fixed issue #10  where IQueryable was being treated as IEnumerable incorrectly

--- src/PagedList/PagedListExtensions.cs
+++ src/PagedList/PagedListExtensions.cs
@@ -6,9 +6,6 @@ namespace PagedList
 	/// <summary>
 	/// Container for extension methods designed to simplify the creation of instances of <see cref="PagedList{T}"/>.
 	/// </summary>
-	/// <remarks>
-	/// Container for extension methods designed to simplify the creation of instances of <see cref="PagedList{T}"/>.
-	/// </remarks>
 	public static class PagedListExtensions
 	{
 		/// <summary>
@@ -21,6 +18,20 @@ namespace PagedList
 		/// <returns>A subset of this collection of objects that can be individually accessed by index and containing metadata about the collection of objects the subset was created from.</returns>
 		/// <seealso cref="PagedList{T}"/>
 		public static IPagedList<T> ToPagedList<T>(this IEnumerable<T> superset, int index, int pageSize)
+		{
+			return new PagedList<T>(superset, index, pageSize);
+		}
+
+		/// <summary>
+		/// Creates a subset of this collection of objects that can be individually accessed by index and containing metadata about the collection of objects the subset was created from.
+		/// </summary>
+		/// <typeparam name="T">The type of object the collection should contain.</typeparam>
+		/// <param name="superset">The collection of objects to be divided into subsets. If the collection implements <see cref="IQueryable{T}"/>, it will be treated as such.</param>
+		/// <param name="index">The index of the subset of objects to be contained by this instance.</param>
+		/// <param name="pageSize">The maximum size of any individual subset.</param>
+		/// <returns>A subset of this collection of objects that can be individually accessed by index and containing metadata about the collection of objects the subset was created from.</returns>
+		/// <seealso cref="PagedList{T}"/>
+		public static IPagedList<T> ToPagedList<T>(this IQueryable<T> superset, int index, int pageSize)
 		{
 			return new PagedList<T>(superset, index, pageSize);
 		}
