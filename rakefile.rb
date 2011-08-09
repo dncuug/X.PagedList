4bd5b21324809205897cda098e83350a5885227e
Troy Goode <troygoode@gmail.com>
2011-08-08 20:02:15 -0400

    bumped to 1.7 and 3.5 to fix a nuget issue where it didnt recognize 1.6 as being equal to 1.6.0.0...

--- rakefile.rb
+++ rakefile.rb
@@ -1,8 +1,8 @@
 require 'albacore' # >= 0.2.7
 require 'fileutils'
 
-pagedlist_version = '1.6.0.0'
-pagedlist_mvc_version = '3.4.0.0'
+pagedlist_version = '1.7'
+pagedlist_mvc_version = '3.5'
 
 task :default => [:build]
 
