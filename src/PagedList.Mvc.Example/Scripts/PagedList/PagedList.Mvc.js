d934f407f6c9829f471049fda79446b8da8b0794
Troy Goode <troygoode@gmail.com>
2011-11-24 22:56:34 -0500

    fixed bug in ajax paging pointed out by Mher Sarkissian

--- src/PagedList.Mvc.Example/Scripts/PagedList/PagedList.Mvc.js
+++ src/PagedList.Mvc.Example/Scripts/PagedList/PagedList.Mvc.js
@@ -1,38 +1,42 @@
 ﻿(function ($) {
-	$.pagedList = function (template, generatePageUrl, options) {
-		var options = $.extend({
-			pagesToDisplay: 10
-		}, options);
+    $.pagedList = function (template, generatePageUrl, options) {
+        var options = $.extend({
+            pagesToDisplay: 10
+        }, options);
 
-		function getRenderOptions(list) {
-			var pagesToDisplay = options.pagesToDisplay;
+        function getRenderOptions(list) {
+            var pagesToDisplay = options.pagesToDisplay;
 
-			var halfOfPagesToDisplay = Math.floor(pagesToDisplay / 2);
-			var arr = [];
-			var start = list.PageNumber - halfOfPagesToDisplay;
-			if (start < 1)
-				start = 1;
-			if ((start + pagesToDisplay) > list.PageCount)
-				start = list.PageCount - pagesToDisplay;
+            var halfOfPagesToDisplay = Math.floor(pagesToDisplay / 2);
+            var arr = [];
+            var start = list.PageNumber - halfOfPagesToDisplay;
+            if (start < 1)
+                start = 1;
 
-			for (var i = start; i <= start + pagesToDisplay; i++)
-				arr.push(i);
+            var end;
+            if ((start + pagesToDisplay) > list.PageCount)
+                end = list.PageCount;
+            else
+                end = start + pagesToDisplay;
 
-			return {
-				pages: arr,
-				showStartEllipses: start != 1,
-				showEndEllipses: arr[arr.length - 1] != list.PageCount
-			};
-		}
+            for (var i = start; i <= end; i++)
+                arr.push(i);
 
-		return {
-			render: function (list) {
-				return $.tmpl(template, {
-					pagedList: list,
-					renderOptions: getRenderOptions(list),
-					generatePageUrl: generatePageUrl
-				});
-			}
-		};
-	};
+            return {
+                pages: arr,
+                showStartEllipses: start != 1,
+                showEndEllipses: arr[arr.length - 1] != list.PageCount
+            };
+        }
+
+        return {
+            render: function (list) {
+                return $.tmpl(template, {
+                    pagedList: list,
+                    renderOptions: getRenderOptions(list),
+                    generatePageUrl: generatePageUrl
+                });
+            }
+        };
+    };
 })(jQuery); 
