7ebaa48dbfb581125f09e6a7d700db668b0c93a8
Troy Goode <troygoode@gmail.com>
2012-08-17 23:37:50 -0400

    Bumped PagedList.Mvc's version.

--- rakefile.rb
+++ rakefile.rb
@@ -70,7 +70,7 @@ nuspec :generate_pagedlistmvc_nuspec do |nuspec|
   nuspec.projectUrl = "http://github.com/TroyGoode/PagedList"
   nuspec.tags = "paging pager page infinitescroll ajax mvc"
   nuspec.dependency "PagedList", PAGEDLIST_VERSION
-  nuspec.file "Content\\**\\*.*", "Content"
+  nuspec.file "..\\PagedList.Mvc4.Example\\Content\\PagedList.css", "Content\\Content"
   nuspec.output_file = "src/PagedList.Mvc/PagedList.Mvc.nuspec"
 end
 
@@ -96,28 +96,13 @@ nugetpack :package_pagedlist => :release do |nuget|
 	nuget.output = './packages/'
 end
 
-#HACK: remove once http://nuget.codeplex.com/workitem/1349 is fixed
-task :prepare_package_pagedlistmvc => :release do
-  content_directory = './src/PagedList.Mvc.Example/Content/'
-  script_directory = './src/PagedList.Mvc.Example/Scripts/PagedList/'
 
-  content_directory_out = './src/PagedList.Mvc/Content/Content/'
-  script_directory_out = './src/PagedList.Mvc/Content/Scripts/PagedList/'
-
-  FileUtils.mkdir_p content_directory_out
-  FileUtils.mkdir_p script_directory_out
-
-  FileUtils.cp content_directory + 'PagedList.css', content_directory_out + 'PagedList.css'
-  FileUtils.cp script_directory + 'PagedList.Mvc.js', script_directory_out + 'PagedList.Mvc.js'
-  FileUtils.cp script_directory + 'PagedList.Mvc.Template.html', script_directory_out + 'PagedList.Mvc.Template.html'
-end
-
-nugetpack :package_pagedlistmvc => :prepare_package_pagedlistmvc do |nuget|
+nugetpack :package_pagedlistmvc => :release do |nuget|
 	nuget.nuspec = './src/PagedList.Mvc/PagedList.Mvc.csproj -Prop Configuration=Release'
 	nuget.output = './packages/'
 end
 
-task :package => [:package_pagedlist, :package_pagedlistmvc] do
+task :pack => [:package_pagedlist, :package_pagedlistmvc] do
 end
 
 nugetpush :push_pagedlist => :package_pagedlist do |nuget|
