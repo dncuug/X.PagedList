8d172070740103aa41047764b710db41be10135f
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2013-12-01 23:43:16 +0200

    NuGet packages updated. Example site updated.

--- src/PagedList.Mvc4.Example/Controllers/HomeController.cs
+++ src/PagedList.Mvc4.Example/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PagedList.Mvc4.Example.Controllers
 {
@@ -11,20 +7,6 @@ namespace PagedList.Mvc4.Example.Controllers
 		public ActionResult Index()
 		{
 			ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-			return View();
-		}
-
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your app description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
 
 			return View();
 		}
--- src/PagedList.Mvc4.Example/Global.asax.cs
+++ src/PagedList.Mvc4.Example/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -17,12 +12,10 @@ namespace PagedList.Mvc4.Example
 		protected void Application_Start()
 		{
 			AreaRegistration.RegisterAllAreas();
-
-			WebApiConfig.Register(GlobalConfiguration.Configuration);
-			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
-			AuthConfig.RegisterAuth();
 		}
 	}
 }
