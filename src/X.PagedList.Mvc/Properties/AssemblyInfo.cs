b6b0f61036541d1350fb9ddbdff11ca77c22f8ba
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2014-01-07 00:59:45 +0200

    Files moved to new directories

--- src/X.PagedList.Mvc/Properties/AssemblyInfo.cs
+++ src/X.PagedList.Mvc/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System;
 using System.Security;
 using System.Reflection;
 using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("PagedList.Mvc")]
+[assembly: AssemblyTitle("X.PagedList.Mvc")]
 [assembly: AssemblyDescription("Asp.Net MVC HtmlHelper method for generating paging control for use with PagedList library.")]
 [assembly: AssemblyCompany("Troy Goode")]
-[assembly: AssemblyProduct("PagedList.Mvc")]
+[assembly: AssemblyProduct("X.PagedList.Mvc")]
 [assembly: AssemblyCopyright("MIT License")]
-[assembly: AssemblyVersion("4.5")]
+[assembly: AssemblyVersion("4.7.0.*")]
 
 //[assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("eb684fee-2094-4833-ae61-f9bfcab34abd")]
 [assembly: AllowPartiallyTrustedCallers()]
-[assembly: AssemblyFileVersion("4.5")]
+[assembly: AssemblyFileVersion("4.7.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- src/X.PagedList/Properties/AssemblyInfo.cs
+++ src/X.PagedList/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PagedList.Core")]
+[assembly: AssemblyTitle("X.PagedList")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Troy Goode")]
 [assembly: AssemblyProduct("PagedList.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("MIT License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.20.0.*")]
+[assembly: AssemblyFileVersion("1.20.0.0")]
