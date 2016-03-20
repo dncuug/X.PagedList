5cb6b5f991458ce706afc305b6c01fe9dcbc39cd
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2016-03-20 12:49:37 -0700

    Update pacakges

--- src/X.PagedList.Mvc.Example/Scripts/_references.js
+++ src/X.PagedList.Mvc.Example/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="jquery-ui-1.11.4.js" />
-/// <reference path="jquery-2.1.4.js" />
+﻿/// <reference path="jquery-2.2.1.js" />
+/// <reference path="jquery-ui-1.11.4.js" />
 /// <reference path="modernizr-2.8.3.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
