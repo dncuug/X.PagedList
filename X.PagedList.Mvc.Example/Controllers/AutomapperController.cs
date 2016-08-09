a258d20cc0b8907b2735242efb379d8e4f4b0e4a
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2016-08-10 01:24:29 +0300

    Update packages

--- X.PagedList.Mvc.Example/Controllers/AutomapperController.cs
+++ X.PagedList.Mvc.Example/Controllers/AutomapperController.cs
@@ -7,109 +7,121 @@ namespace X.PagedList.Mvc.Example.Controllers
 {
     public class AutomapperController : Controller
     {
-		public ActionResult Index(int? page)
-		{
-			Mapper.CreateMap<Order, OrderViewModel>(); // create mapping between Order and OrderViewModel
-			const int pageSize = 2;
+        public class OrderOrderViewModelProfile : Profile
+        {
+            public override string ProfileName => "Mapping between Order and OrderViewModel";
 
-			var orders = GetAllOrdersFromDatabase();
-			var pagedOrders = orders.ToPagedList(page ?? 1, pageSize);
+            public OrderOrderViewModelProfile()
+            {
+                CreateMap<Order, OrderViewModel>();
+            }
+        }
 
-			var viewmodel = Mapper.Map<IEnumerable<Order>, IEnumerable<OrderViewModel>>(pagedOrders.ToArray()); // convert all the Orders in the paged list into viewmodels
-			var pagedViewmodel = new StaticPagedList<OrderViewModel>(viewmodel, pagedOrders.GetMetaData()); // reconsitute a new IPagedList containing the viewmodels
 
-			return View(pagedViewmodel);
-		}
+        public ActionResult Index(int? page)
+        {
 
-		public IEnumerable<Order> GetAllOrdersFromDatabase()
-		{
-			var jerrysOrder = new Order(
-				new Customer { Name = "Jerry Seinfeld" },
-				new[] { new OrderLineItem(new Product("Superman Underoos", 17.99m), 7) }
-			);
-			var georgesOrder = new Order(
-				new Customer { Name = "George Costanza" },
-				new[] { new OrderLineItem(new Product("Bosco", 4.99m), 15) }
-			);
-			var kramersOrder = new Order(
-				new Customer { Name = "Cosmo Kramer" },
-				new[] { new OrderLineItem(new Product("Door Knobs", 7.95m), 30) }
-			);
-			var elainesOrder = new Order(
-				new Customer { Name = "Elaine Benes" },
-				new[] { new OrderLineItem(new Product("Sponge", 6.95m), 500) }
-			);
-			var newmansOrder = new Order(
-				new Customer { Name = "Newman" },
-				new[] { new OrderLineItem(new Product("Dinosaur DNA", 1500000m), 1) }
-			);
+            Mapper.Initialize(x => x.AddProfile<OrderOrderViewModelProfile>()); // create mapping between Order and OrderViewModel
+            const int pageSize = 2;
 
-			var orders = new List<Order>();
-			orders.Add(jerrysOrder);
-			orders.Add(georgesOrder);
-			orders.Add(kramersOrder);
-			orders.Add(elainesOrder);
-			orders.Add(newmansOrder);
-			return orders;
-		}
+            var orders = GetAllOrdersFromDatabase();
+            var pagedOrders = orders.ToPagedList(page ?? 1, pageSize);
 
-		public class OrderViewModel
-		{
-			public string CustomerName { get; set; }
-			public decimal Total { get; set; }
-		}
+            var viewmodel = Mapper.Map<IEnumerable<Order>, IEnumerable<OrderViewModel>>(pagedOrders.ToArray()); // convert all the Orders in the paged list into viewmodels
+            var pagedViewmodel = new StaticPagedList<OrderViewModel>(viewmodel, pagedOrders.GetMetaData()); // reconsitute a new IPagedList containing the viewmodels
 
-		// Example classes from: http://automapper.codeplex.com/wikipage?title=Flattening&referringTitle=Home
+            return View(pagedViewmodel);
+        }
 
-		public class Customer
-		{
-			public string Name { get; set; }
-		}
+        public IEnumerable<Order> GetAllOrdersFromDatabase()
+        {
+            var jerrysOrder = new Order(
+                new Customer { Name = "Jerry Seinfeld" },
+                new[] { new OrderLineItem(new Product("Superman Underoos", 17.99m), 7) }
+            );
+            var georgesOrder = new Order(
+                new Customer { Name = "George Costanza" },
+                new[] { new OrderLineItem(new Product("Bosco", 4.99m), 15) }
+            );
+            var kramersOrder = new Order(
+                new Customer { Name = "Cosmo Kramer" },
+                new[] { new OrderLineItem(new Product("Door Knobs", 7.95m), 30) }
+            );
+            var elainesOrder = new Order(
+                new Customer { Name = "Elaine Benes" },
+                new[] { new OrderLineItem(new Product("Sponge", 6.95m), 500) }
+            );
+            var newmansOrder = new Order(
+                new Customer { Name = "Newman" },
+                new[] { new OrderLineItem(new Product("Dinosaur DNA", 1500000m), 1) }
+            );
 
-		public class Product
-		{
-			public Product(string name, decimal price)
-			{
-				Name = name;
-				Price = price;
-			}
+            var orders = new List<Order>();
+            orders.Add(jerrysOrder);
+            orders.Add(georgesOrder);
+            orders.Add(kramersOrder);
+            orders.Add(elainesOrder);
+            orders.Add(newmansOrder);
+            return orders;
+        }
 
-			public string Name { get; private set; }
-			public decimal Price { get; private set; }
-		}
+        public class OrderViewModel
+        {
+            public string CustomerName { get; set; }
+            public decimal Total { get; set; }
+        }
 
-		public class OrderLineItem
-		{
-			public OrderLineItem(Product product, int quantity)
-			{
-				OrderedProduct = product;
-				Quantity = quantity;
-			}
+        // Example classes from: http://automapper.codeplex.com/wikipage?title=Flattening&referringTitle=Home
 
-			public Product OrderedProduct { get; private set; }
-			public int Quantity { get; private set; }
+        public class Customer
+        {
+            public string Name { get; set; }
+        }
 
-			public decimal GetTotal()
-			{
-				return OrderedProduct.Price * Quantity;
-			}
-		}
+        public class Product
+        {
+            public Product(string name, decimal price)
+            {
+                Name = name;
+                Price = price;
+            }
 
-		public class Order
-		{
-			public Order(Customer customer, IEnumerable<OrderLineItem> lineItems)
-			{
-				Customer = customer;
-				LineItems = lineItems;
-			}
+            public string Name { get; private set; }
+            public decimal Price { get; private set; }
+        }
 
-			public Customer Customer { get; private set; }
-			public IEnumerable<OrderLineItem> LineItems { get; private set; }
+        public class OrderLineItem
+        {
+            public OrderLineItem(Product product, int quantity)
+            {
+                OrderedProduct = product;
+                Quantity = quantity;
+            }
 
-			public decimal GetTotal()
-			{
-				return LineItems.Sum(x => x.GetTotal());
-			}
-		}
+            public Product OrderedProduct { get; private set; }
+            public int Quantity { get; private set; }
+
+            public decimal GetTotal()
+            {
+                return OrderedProduct.Price * Quantity;
+            }
+        }
+
+        public class Order
+        {
+            public Order(Customer customer, IEnumerable<OrderLineItem> lineItems)
+            {
+                Customer = customer;
+                LineItems = lineItems;
+            }
+
+            public Customer Customer { get; private set; }
+            public IEnumerable<OrderLineItem> LineItems { get; private set; }
+
+            public decimal GetTotal()
+            {
+                return LineItems.Sum(x => x.GetTotal());
+            }
+        }
     }
 }
--- X.PagedList.Mvc.Example/Scripts/_references.js
+++ X.PagedList.Mvc.Example/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="jquery-3.0.0.js" />
-/// <reference path="jquery-ui-1.11.4.js" />
+﻿/// <reference path="jquery-3.1.0.js" />
+/// <reference path="jquery-ui-1.12.0.js" />
 /// <reference path="modernizr-2.8.3.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
