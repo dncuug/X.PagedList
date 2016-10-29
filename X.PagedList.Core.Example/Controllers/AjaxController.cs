fd36c25a096e3a61e0aa9b07c18232eb2a4948ed
Sasha <sashapvt@gmail.com>
2016-10-29 22:27:19 +0300

    Ajax list example is ported

---
+++ X.PagedList.Core.Example/Controllers/AjaxController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace X.PagedList.Core.Example.Controllers
+{
+    public class AjaxController : BaseController
+	{
+		// Ajax Paging
+		public ViewResult Index()
+		{
+			return View();
+		}
+
+        // Ajax Paging (cont'd)
+        [HttpGet]
+        public ActionResult AjaxPage(int? page)
+		{
+			var listPaged = GetPagedNames(page); // GetPagedNames is found in BaseController
+			if (listPaged == null)
+				return NotFound();
+
+			return Json(new
+			{
+				names = listPaged.ToArray(),
+				pager = listPaged.GetMetaData()
+			});
+		}
+    }
+}
