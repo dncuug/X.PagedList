f7a01850f31e8f333a0c8231339d42acfb73dbb1
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2018-09-26 22:27:21 +0300

    update packages

--- examples/X.PagedList.Mvc.Example/Scripts/_references.js
+++ examples/X.PagedList.Mvc.Example/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-3.2.1.js" />
+﻿/// <reference path="jquery-3.3.1.js" />
 /// <reference path="jquery-ui-1.12.1.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
