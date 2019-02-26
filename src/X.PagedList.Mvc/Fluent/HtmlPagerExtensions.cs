1e628d77e9b7d3821ae6f3f1abb9b816dcef73c1
Ivan Grek <kvant@outlook.com>
2019-02-26 09:37:18 +0200

    Add ability to build PagedList fluently, add creation based on Partial View, fix #85, #136, #58, #124

---
+++ src/X.PagedList.Mvc/Fluent/HtmlPagerExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace X.PagedList.Mvc.Fluent
+{
+    using System.Linq;
+    using System.Web;
+
+    public static class HtmlPagerExtensions
+    {
+        public static IHtmlString Pager(this System.Web.Mvc.HtmlHelper htmlHelper)
+        {
+            return new HtmlPagerBuilder(htmlHelper, Enumerable.Empty<object>().ToPagedList()).Build();
+        }
+
+        public static IHtmlPagerBuilder Pager(this System.Web.Mvc.HtmlHelper htmlHelper, IPagedList list)
+        {
+            return new HtmlPagerBuilder(htmlHelper, list);
+        }
+    }
+}
---
+++ src/X.PagedList.Mvc/Fluent/IHtmlPagerBuilder.cs
@@ -0,0 +1,47 @@
+﻿namespace X.PagedList.Mvc.Fluent
+{
+    using System;
+    using System.Web;
+    using Common;
+
+    public interface IHtmlPagerBuilder
+    {
+        IHtmlPagerBuilder Url(Func<int, string> builder);
+
+        IHtmlPagerBuilder DisplayLinkToFirstPage(PagedListDisplayMode displayMode = PagedListDisplayMode.Always);
+
+        IHtmlPagerBuilder DisplayLinkToLastPage(PagedListDisplayMode displayMode = PagedListDisplayMode.Always);
+
+        IHtmlPagerBuilder DisplayLinkToPreviousPage(PagedListDisplayMode displayMode = PagedListDisplayMode.Always);
+
+        IHtmlPagerBuilder DisplayLinkToNextPage(PagedListDisplayMode displayMode = PagedListDisplayMode.Always);
+
+        IHtmlPagerBuilder DisplayLinkToIndividualPages(bool displayMode = true);
+
+        IHtmlPagerBuilder DisplayPageCountAndCurrentLocation(bool displayMode = true);
+
+        IHtmlPagerBuilder DisplayEllipsesWhenNotShowingAllPageNumbers(bool displayMode = true);
+
+        IHtmlPagerBuilder MaximumPageNumbersToDisplay(int pageNumbers);
+
+        IHtmlPagerBuilder WithPartialView(string partialViewName);
+
+        IHtmlString Classic();
+
+        IHtmlString ClassicPlusFirstAndLast();
+
+        IHtmlString Minimal();
+
+        IHtmlString MinimalWithPageCountText();
+
+        IHtmlString MinimalWithItemCountText();
+
+        IHtmlString PageNumbersOnly();
+
+        IHtmlString OnlyShowFivePagesAtATime();
+
+        IHtmlString Build();
+
+        IHtmlString Build(PagedListRenderOptions options);
+    }
+}
