ffcaccdacf852286a0099475221697ab05108e4a
Martijn Wolfs <martijn.wolfs@orbissoftwarebenelux.onmicrosoft.com>
2019-11-28 11:41:44 +0100

    Add unit test for X.PagedList.EF
    Add test methods in a new unit test project for X.PagedList.EF to
    demonstrate the "ArgumentException" described in issue #169.

---
+++ tests/X.PagedList.EF.Tests/TestContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace X.PagedList.EF.Tests
+{
+    class TestContext: DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        public TestContext()
+        {
+            Database.SetInitializer(new TestContextDBInitializer());
+        }
+
+    }
+
+    class TestContextDBInitializer : DropCreateDatabaseAlways<TestContext>
+    {
+        protected override void Seed(TestContext context)
+        {
+            // Create a couple of blog entries.
+            IList<Blog> defaultBlogs = new List<Blog>();
+            for (int i = 1; i <= 10; i++)
+            {
+                defaultBlogs.Add(new Blog() {BlogId = i, Name = $"Blog {i}", Url = $"http://blogs.com/{i}"});
+            }
+
+            context.Blogs.AddRange(defaultBlogs);
+
+            base.Seed(context);
+        }
+    }
+
+    class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set;  }
+    }
+}
