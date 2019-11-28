09ff8bee310eda9ab7a6a0617833d3f94cfe0fa8
Martijn Wolfs <martijn.wolfs@orbissoftwarebenelux.onmicrosoft.com>
2019-11-28 12:26:18 +0100

    Split classes into seperate files

--- tests/X.PagedList.EF.Tests/TestContext.cs
+++ tests/X.PagedList.EF.Tests/TestContext.cs
@@ -13,30 +13,5 @@ namespace X.PagedList.EF.Tests
         {
             Database.SetInitializer(new TestContextDBInitializer());
         }
-
-    }
-
-    class TestContextDBInitializer : DropCreateDatabaseAlways<TestContext>
-    {
-        protected override void Seed(TestContext context)
-        {
-            // Create a couple of blog entries.
-            IList<Blog> defaultBlogs = new List<Blog>();
-            for (int i = 1; i <= 10; i++)
-            {
-                defaultBlogs.Add(new Blog() {BlogId = i, Name = $"Blog {i}", Url = $"http://blogs.com/{i}"});
-            }
-
-            context.Blogs.AddRange(defaultBlogs);
-
-            base.Seed(context);
-        }
-    }
-
-    class Blog
-    {
-        public int BlogId { get; set; }
-        public string Name { get; set; }
-        public string Url { get; set;  }
     }
 }
---
+++ tests/X.PagedList.EF.Tests/TestContextDBInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace X.PagedList.EF.Tests
+{
+    class TestContextDBInitializer : DropCreateDatabaseAlways<TestContext>
+    {
+        protected override void Seed(TestContext context)
+        {
+            // Create a couple of blog entries.
+            IList<Blog> defaultBlogs = new List<Blog>();
+            for (int i = 1; i <= 10; i++)
+            {
+                defaultBlogs.Add(new Blog() {BlogId = i, Name = $"Blog {i}", Url = $"http://blogs.com/{i}"});
+            }
+
+            context.Blogs.AddRange(defaultBlogs);
+
+            base.Seed(context);
+        }
+    }
+}
