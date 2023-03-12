785309e0967f71daa38f372e402d3a61e1a5f089
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2023-03-12 23:18:41 +0200

    Update project settings

--- src/X.PagedList.EF/PagedListExtensions.cs
+++ src/X.PagedList.EF/PagedListExtensions.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 namespace X.PagedList.EF;
 
 /// <summary>
-/// Container for extension methods designed to simplify the creation of instances of <see cref="PagedList{T}"/>.
+/// EntityFramework extension methods designed to simplify the creation of instances of <see cref="PagedList{T}"/>.
 /// </summary>
 [PublicAPI]
 public static class PagedListExtensions
@@ -52,8 +52,7 @@ public static class PagedListExtensions
             }
             else
             {
-                totalCount = await superset.CountAsync().ConfigureAwait(false);
-                ;
+                totalCount = await superset.CountAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
             }
 
             if (totalCount > 0)
@@ -68,4 +67,21 @@ public static class PagedListExtensions
 
         return new StaticPagedList<T>(subset, pageNumber, pageSize, totalCount);
     }
+
+    /// <summary>
+    /// Async creates a subset of this collection of objects that can be individually accessed by index and
+    /// containing metadata about the collection of objects the subset was created from.
+    /// </summary>
+    /// <typeparam name="T">The type of object the collection should contain.</typeparam>
+    /// <param name="superset">The collection of objects to be divided into subsets. If the collection implements <see cref="IQueryable{T}"/>, it will be treated as such.</param>
+    /// <param name="pageNumber">The one-based index of the subset of objects to be contained by this instance.</param>
+    /// <param name="pageSize">The maximum size of any individual subset.</param>
+    /// <param name="totalSetCount">The total size of set</param>
+    /// <returns>
+    /// A subset of this collection of objects that can be individually accessed by index and containing metadata
+    /// about the collection of objects the subset was created from.
+    /// </returns>
+    /// <seealso cref="PagedList{T}"/>
+    public static Task<IPagedList<T>> ToPagedListAsync<T>(this IQueryable<T> superset, int pageNumber, int pageSize, int? totalSetCount = null) =>
+        ToPagedListAsync(superset, pageNumber, pageSize, totalSetCount, CancellationToken.None);
 }
