e929181454eea5c173e947536b4a1ae18daaa2d5
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2023-03-12 22:55:46 +0200

    Restore X.PagedList.EF project

---
+++ src/X.PagedList.EF/PagedListExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace X.PagedList.EF;
+
+/// <summary>
+/// Container for extension methods designed to simplify the creation of instances of <see cref="PagedList{T}"/>.
+/// </summary>
+[PublicAPI]
+public static class PagedListExtensions
+{
+    /// <summary>
+    /// Async creates a subset of this collection of objects that can be individually accessed by index and
+    /// containing metadata about the collection of objects the subset was created from.
+    /// </summary>
+    /// <typeparam name="T">The type of object the collection should contain.</typeparam>
+    /// <param name="superset">The collection of objects to be divided into subsets. If the collection implements <see cref="IQueryable{T}"/>, it will be treated as such.</param>
+    /// <param name="pageNumber">The one-based index of the subset of objects to be contained by this instance.</param>
+    /// <param name="pageSize">The maximum size of any individual subset.</param>
+    /// <param name="totalSetCount">The total size of set</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>
+    /// A subset of this collection of objects that can be individually accessed by index and containing metadata
+    /// about the collection of objects the subset was created from.
+    /// </returns>
+    /// <seealso cref="PagedList{T}"/>
+    public static async Task<IPagedList<T>> ToPagedListAsync<T>(this IQueryable<T> superset, int pageNumber, int pageSize, int? totalSetCount, CancellationToken cancellationToken)
+    {
+        if (pageNumber < 1)
+        {
+            throw new ArgumentOutOfRangeException($"pageNumber = {pageNumber}. PageNumber cannot be below 1.");
+        }
+
+        if (pageSize < 1)
+        {
+            throw new ArgumentOutOfRangeException($"pageSize = {pageSize}. PageSize cannot be less than 1.");
+        }
+
+        var subset = new List<T>();
+        var totalCount = 0;
+
+        if (superset != null)
+        {
+            if (totalSetCount.HasValue)
+            {
+                totalCount = totalSetCount.Value;
+            }
+            else
+            {
+                totalCount = await superset.CountAsync().ConfigureAwait(false);
+                ;
+            }
+
+            if (totalCount > 0)
+            {
+                subset.AddRange(
+                    (pageNumber == 1)
+                        ? await superset.Skip(0).Take(pageSize).ToListAsync(cancellationToken).ConfigureAwait(false)
+                        : await superset.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(cancellationToken).ConfigureAwait(false)
+                );
+            }
+        }
+
+        return new StaticPagedList<T>(subset, pageNumber, pageSize, totalCount);
+    }
+}
