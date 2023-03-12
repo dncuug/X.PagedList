754bb16b59c7dbb0c4865e2a74019988401c4087
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2023-03-12 22:56:43 +0200

    Remove fake ToListAsync methods

--- src/X.PagedList/PagedListExtensions.cs
+++ src/X.PagedList/PagedListExtensions.cs
@@ -233,50 +233,6 @@ public static class PagedListExtensions
     /// containing metadata about the collection of objects the subset was created from.
     /// </summary>
     /// <typeparam name="T">The type of object the collection should contain.</typeparam>
-    /// <param name="superset">
-    /// The collection of objects to be divided into subsets. If the collection
-    /// implements <see cref="IEnumerable{T}"/>, it will be treated as such.
-    /// </param>
-    /// <returns>
-    /// A subset of this collection of objects that can be individually accessed by index and containing
-    /// metadata about the collection of objects the subset was created from.
-    /// </returns>
-    /// <seealso cref="PagedList{T}"/>
-    public static async Task<List<T>> ToListAsync<T>(this IEnumerable<T> superset)
-    {
-        return await ToListAsync(superset, CancellationToken.None);
-    }
-
-    /// <summary>
-    /// Async creates a subset of this collection of objects that can be individually accessed by index and
-    /// containing metadata about the collection of objects the subset was created from.
-    /// </summary>
-    /// <typeparam name="T">The type of object the collection should contain.</typeparam>
-    /// <param name="superset">
-    /// The collection of objects to be divided into subsets. If the collection
-    /// implements <see cref="IEnumerable{T}"/>, it will be treated as such.
-    /// </param>
-    /// <param name="cancellationToken"></param>
-    /// <returns>
-    /// A subset of this collection of objects that can be individually accessed by index and containing metadata
-    /// about the collection of objects the subset was created from.
-    /// </returns>
-    /// <seealso cref="PagedList{T}"/>
-    public static async Task<List<T>> ToListAsync<T>(this IEnumerable<T> superset, CancellationToken cancellationToken)
-    {
-        return await Task.Run(superset.ToList, cancellationToken);
-    }
-
-    public static async Task<int> CountAsync<T>(this IEnumerable<T> superset, CancellationToken cancellationToken)
-    {
-        return await Task.Run(superset.Count, cancellationToken);
-    }
-
-    /// <summary>
-    /// Async creates a subset of this collection of objects that can be individually accessed by index and
-    /// containing metadata about the collection of objects the subset was created from.
-    /// </summary>
-    /// <typeparam name="T">The type of object the collection should contain.</typeparam>
     /// <param name="superset">The collection of objects to be divided into subsets. If the collection implements <see cref="IQueryable{T}"/>, it will be treated as such.</param>
     /// <param name="pageNumber">The one-based index of the subset of objects to be contained by this instance.</param>
     /// <param name="pageSize">The maximum size of any individual subset.</param>
@@ -302,7 +258,6 @@ public static class PagedListExtensions
         var subset = new List<T>();
         var totalCount = 0;
 
-
         if (superset != null)
         {
             if (totalSetCount.HasValue)
@@ -311,15 +266,15 @@ public static class PagedListExtensions
             }
             else
             {
-                totalCount = await superset.CountAsync(cancellationToken).ConfigureAwait(false);
+                totalCount = superset.Count();
             }
 
             if (totalCount > 0)
             {
                 subset.AddRange(
                     (pageNumber == 1)
-                        ? await superset.Skip(0).Take(pageSize).ToListAsync(cancellationToken).ConfigureAwait(false)
-                        : await superset.Skip(((pageNumber - 1) * pageSize)).Take(pageSize).ToListAsync(cancellationToken).ConfigureAwait(false)
+                        ? superset.Skip(0).Take(pageSize).ToList()
+                        : superset.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList()
                 );
             }
         }
