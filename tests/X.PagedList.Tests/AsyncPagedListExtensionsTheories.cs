4938e073be73cce284e14208383bcb05f3088c4b
Aghogho Bernard <aghoghobernard@gmail.com>
2023-03-13 06:46:20 -0400

    Add Unit test for TotalSetCount

--- tests/X.PagedList.Tests/AsyncPagedListExtensionsTheories.cs
+++ tests/X.PagedList.Tests/AsyncPagedListExtensionsTheories.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoFixture;
 using Xunit;
 
 namespace X.PagedList.Tests;
@@ -33,6 +34,31 @@ public class AsyncPagedListExtensionsTheories
         Assert.Equal(expectedCount, pagedBlogs.Count);
         Assert.Equal(1, pagedBlogs.PageNumber);
         Assert.Equal(pageSize, pagedBlogs.PageSize);
+    }
+    
+    [Theory]
+    [InlineData(1000, 100, 10, null)]
+    [InlineData(1000, 200, 5, 3)]
+    [InlineData(1000, 300, 4, 4)]
+    public async Task ToListAsync_ForQueryable_With_TotalSetCount_Works(int superSetTotalCount, int pageSize,
+        int expectedCount, int? pageNumber = null)
+    {
+        var fixture = new Fixture();
+        
+        pageNumber = pageNumber.HasValue == false ? 0 : pageNumber;
+        var listPageNumber = pageNumber != 0 ? pageNumber - 1 : pageNumber;
+        var xListPageNumber = pageNumber == 0 ? 1 : pageNumber;
+        
+        var superset = fixture.CreateMany<Blog>(superSetTotalCount).AsQueryable();
+        var pageOfSuperSet = superset.Skip(listPageNumber.Value * pageSize).Take(pageSize).ToList();
+        
+        var pagedBlogs = await superset.ToPagedListAsync(xListPageNumber, pageSize, superset.Count());
+        
+        Assert.Equal(expectedCount, pagedBlogs.PageCount);
+        Assert.Equal(xListPageNumber, pagedBlogs.PageNumber);
+        Assert.Equal(pageSize, pagedBlogs.PageSize);
+        Assert.Equal(pageOfSuperSet.Count(), pagedBlogs.Count);
+        Assert.Equal(pageOfSuperSet.First().Name, pagedBlogs.First().Name);
     }
 
     private static IQueryable<Blog> BuildBlogList()
