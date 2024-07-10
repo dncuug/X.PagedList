6a79a71b13cbd178de9c70fec4f219ca98c0b575
Adrian Schmutzler <adrian.schmutzler@hansweber.de>
2024-07-10 10:04:01 +0200

    X.PagedList: PagedListExtensions: Add missing variant for IEnumerable
    This has an async-over-sync implementation, but no native sync one. Add it.

--- src/X.PagedList/PagedListExtensions.cs
+++ src/X.PagedList/PagedListExtensions.cs
@@ -148,6 +148,32 @@ public static class PagedListExtensions
     /// metadata about the collection of objects the subset was created from.
     /// </summary>
     /// <typeparam name="T">The type of object the collection should contain.</typeparam>
+    /// <param name="superset">
+    /// The collection of objects to be divided into subsets. If the collection
+    /// implements <see cref="IEnumerable{T}"/>, it will be treated as such.
+    /// </param>
+    /// <param name="pageNumber">The one-based index of the subset of objects to be contained by this instance.</param>
+    /// <param name="pageSize">The maximum size of any individual subset.</param>
+    /// <param name="totalSetCount">The total size of set</param>
+    /// <returns>
+    /// A subset of this collection of objects that can be individually accessed by index and containing metadata
+    /// about the collection of objects the subset was created from.
+    /// </returns>
+    public static IPagedList<T> ToPagedList<T>(this IEnumerable<T> superset, int pageNumber, int pageSize, int? totalSetCount)
+    {
+        if (superset == null)
+        {
+            throw new ArgumentNullException(nameof(superset));
+        }
+
+        return ToPagedList(superset.AsQueryable(), pageNumber, pageSize, totalSetCount);
+    }
+
+    /// <summary>
+    /// Creates a subset of this collection of objects that can be individually accessed by index and containing
+    /// metadata about the collection of objects the subset was created from.
+    /// </summary>
+    /// <typeparam name="T">The type of object the collection should contain.</typeparam>
     /// <typeparam name="TKey">Type For Compare</typeparam>
     /// <param name="superset">
     /// The collection of objects to be divided into subsets. If the collection
