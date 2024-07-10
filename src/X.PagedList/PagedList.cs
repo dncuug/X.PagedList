be8d1c7597acb5604f55a55fdb969ebf843e96c3
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2024-07-10 22:00:46 +0300

    Separate different PagedList implementation on different files

--- src/X.PagedList/PagedList.cs
+++ src/X.PagedList/PagedList.cs
@@ -2,58 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 
 namespace X.PagedList;
-
-[PublicAPI]
-public class PagedList<T, TKey> : BasePagedList<T>
-{
-    /// <summary>
-    /// Initializes a new instance of the <see cref="PagedList{T}"/> class that divides the supplied superset into
-    /// subsets the size of the supplied pageSize. The instance then only contains the objects contained in the
-    /// subset specified by index.
-    /// </summary>
-    /// <param name="superset">
-    /// The collection of objects to be divided into subsets. If the collection
-    /// implements <see cref="IQueryable{T}"/>, it will be treated as such.
-    /// </param>
-    /// <param name="keySelector">Expression for Order</param>
-    /// <param name="pageNumber">
-    /// The one-based index of the subset of objects to be contained by this instance.
-    /// </param>
-    /// <param name="pageSize">The maximum size of any individual subset.</param>
-    /// <exception cref="ArgumentOutOfRangeException">The specified index cannot be less than zero.</exception>
-    /// <exception cref="ArgumentOutOfRangeException">The specified page size cannot be less than one.</exception>
-    public PagedList(IQueryable<T>? superset, Expression<Func<T, TKey>> keySelector, int pageNumber, int pageSize)
-        : base(pageNumber, pageSize, superset?.Count() ?? 0)
-    {
-        // add items to internal list
-        if (superset != null && TotalItemCount > 0)
-        {
-            InitSubset(superset, keySelector.Compile(), pageNumber, pageSize);
-        }
-    }
-
-    public PagedList(IQueryable<T>? superset, Func<T, TKey> keySelectorMethod, int pageNumber, int pageSize)
-        : base(pageNumber, pageSize, superset?.Count() ?? 0)
-    {
-        if (superset != null && TotalItemCount > 0)
-        {
-            InitSubset(superset, keySelectorMethod, pageNumber, pageSize);
-        }
-    }
-
-    private void InitSubset(IEnumerable<T> superset, Func<T, TKey> keySelectorMethod, int pageNumber, int pageSize)
-    {
-        // add items to internal list
-
-        var skip = (pageNumber - 1) * pageSize;
-        var items = superset.OrderBy(keySelectorMethod).Skip(skip).Take(pageSize).ToList();
-
-        Subset.AddRange(items);
-    }
-}
 
 /// <summary>
 /// Represents a subset of a collection of objects that can be individually accessed by index and containing
@@ -68,6 +18,7 @@ public class PagedList<T, TKey> : BasePagedList<T>
 /// <seealso cref="BasePagedList{T}"/>
 /// <seealso cref="StaticPagedList{T}"/>
 /// <seealso cref="List{T}"/>
+[PublicAPI]
 public class PagedList<T> : BasePagedList<T>
 {
     /// <summary>
---
+++ src/X.PagedList/PagedListWithKey.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using JetBrains.Annotations;
+
+namespace X.PagedList;
+
+/// <summary>
+/// Represents a subset of a collection of objects that can be individually accessed by index and containing
+/// metadata about the superset collection of objects this subset was created from.
+/// </summary>
+/// <remarks>
+/// Represents a subset of a collection of objects that can be individually accessed by index and containing
+/// metadata about the superset collection of objects this subset was created from.
+/// </remarks>
+/// <typeparam name="T">The type of object the collection should contain.</typeparam>
+/// <typeparam name="TKey"></typeparam>
+[PublicAPI]
+public class PagedList<T, TKey> : BasePagedList<T>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PagedList{T}"/> class that divides the supplied superset into
+    /// subsets the size of the supplied pageSize. The instance then only contains the objects contained in the
+    /// subset specified by index.
+    /// </summary>
+    /// <param name="superset">
+    /// The collection of objects to be divided into subsets. If the collection
+    /// implements <see cref="IQueryable"/>, it will be treated as such.
+    /// </param>
+    /// <param name="keySelector">Expression for Order</param>
+    /// <param name="pageNumber">
+    /// The one-based index of the subset of objects to be contained by this instance.
+    /// </param>
+    /// <param name="pageSize">The maximum size of any individual subset.</param>
+    /// <exception cref="ArgumentOutOfRangeException">The specified index cannot be less than zero.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">The specified page size cannot be less than one.</exception>
+    public PagedList(IQueryable<T>? superset, Expression<Func<T, TKey>> keySelector, int pageNumber, int pageSize)
+        : base(pageNumber, pageSize, superset?.Count() ?? 0)
+    {
+        // add items to internal list
+        if (superset != null && TotalItemCount > 0)
+        {
+            InitSubset(superset, keySelector.Compile(), pageNumber, pageSize);
+        }
+    }
+
+    public PagedList(IQueryable<T>? superset, Func<T, TKey> keySelectorMethod, int pageNumber, int pageSize)
+        : base(pageNumber, pageSize, superset?.Count() ?? 0)
+    {
+        if (superset != null && TotalItemCount > 0)
+        {
+            InitSubset(superset, keySelectorMethod, pageNumber, pageSize);
+        }
+    }
+
+    private void InitSubset(IEnumerable<T> superset, Func<T, TKey> keySelectorMethod, int pageNumber, int pageSize)
+    {
+        // add items to internal list
+
+        var skip = (pageNumber - 1) * pageSize;
+        var items = superset.OrderBy(keySelectorMethod).Skip(skip).Take(pageSize).ToList();
+
+        Subset.AddRange(items);
+    }
+}
