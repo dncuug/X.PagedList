48b80ac388a028bddd5a4fa786c1874915afcd5e
Adrian Schmutzler <adrian.schmutzler@hansweber.de>
2024-07-12 15:06:07 +0200

    X.PagedList: PagedList: Improve creation of subset
    This addresses two aspects:
    
      1. Instead of using AddRange(), just saving the existing List is cheaper.
    
      2. There is an important difference between an Expression and a method
         delegate (Func<Tin, Tout>). In almost every case, you will need to
         use Expressions with IQueryable and method delegates with IEnumerable.
    
         In the previous implementation, we threw away the Expression by using
         Compile(). With a real IQueryable, this would mean that processing is moved
         from the DB to client evaluation for anything beyond that point. Hence,
         we change this to use the proper OrderBy overload for Expressions now with
         IQueryable. When a method delegate is provided, we directly assume
         IEnumerable.
    
         Note that this is NO breaking change, since IQueryable implements
         IEnumerable. However, for a real IQueryable this will now actually work
         on the DB e.g. in an EF Core scenario.

--- src/X.PagedList/PagedList.cs
+++ src/X.PagedList/PagedList.cs
@@ -18,7 +18,7 @@ public class PagedList<T, TKey> : BasePagedList<T>
     /// The collection of objects to be divided into subsets. If the collection
     /// implements <see cref="IQueryable{T}"/>, it will be treated as such.
     /// </param>
-    /// <param name="keySelector">Expression for Order</param>
+    /// <param name="keySelectorExpression">Expression for OrderBy</param>
     /// <param name="pageNumber">
     /// The one-based index of the subset of objects to be contained by this instance.
     /// </param>
@@ -26,7 +26,7 @@ public class PagedList<T, TKey> : BasePagedList<T>
     /// <exception cref="ArgumentNullException"></exception>
     /// <exception cref="ArgumentOutOfRangeException">The specified index cannot be less than zero.</exception>
     /// <exception cref="ArgumentOutOfRangeException">The specified page size cannot be less than one.</exception>
-    public PagedList(IQueryable<T> superset, Expression<Func<T, TKey>> keySelector, int pageNumber, int pageSize)
+    public PagedList(IQueryable<T> superset, Expression<Func<T, TKey>> keySelectorExpression, int pageNumber, int pageSize)
         : base(pageNumber, pageSize, superset.Count())
     {
         if (superset is null)
@@ -37,11 +37,30 @@ public class PagedList<T, TKey> : BasePagedList<T>
         // add items to internal list
         if (TotalItemCount > 0)
         {
-            InitSubset(superset, keySelector.Compile(), pageNumber, pageSize);
+            var skip = (pageNumber - 1) * pageSize;
+
+            Subset = superset.OrderBy(keySelectorExpression).Skip(skip).Take(pageSize).ToList();
         }
     }
 
-    public PagedList(IQueryable<T> superset, Func<T, TKey> keySelectorMethod, int pageNumber, int pageSize)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PagedList{T}"/> class that divides the supplied superset into
+    /// subsets the size of the supplied pageSize. The instance then only contains the objects contained in the
+    /// subset specified by index.
+    /// </summary>
+    /// <param name="superset">
+    /// The collection of objects to be divided into subsets. If the collection
+    /// implements <see cref="IEnumerable{T}"/>, it will be treated as such.
+    /// </param>
+    /// <param name="keySelectorMethod">Function delegate for OrderBy</param>
+    /// <param name="pageNumber">
+    /// The one-based index of the subset of objects to be contained by this instance.
+    /// </param>
+    /// <param name="pageSize">The maximum size of any individual subset.</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="ArgumentOutOfRangeException">The specified index cannot be less than zero.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">The specified page size cannot be less than one.</exception>
+    public PagedList(IEnumerable<T> superset, Func<T, TKey> keySelectorMethod, int pageNumber, int pageSize)
         : base(pageNumber, pageSize, superset.Count())
     {
         if (superset is null)
@@ -52,18 +71,10 @@ public class PagedList<T, TKey> : BasePagedList<T>
         // add items to internal list
         if (TotalItemCount > 0)
         {
-            InitSubset(superset, keySelectorMethod, pageNumber, pageSize);
+            var skip = (pageNumber - 1) * pageSize;
+
+            Subset = superset.OrderBy(keySelectorMethod).Skip(skip).Take(pageSize).ToList();
         }
-    }
-
-    private void InitSubset(IEnumerable<T> superset, Func<T, TKey> keySelectorMethod, int pageNumber, int pageSize)
-    {
-        // add items to internal list
-
-        var skip = (pageNumber - 1) * pageSize;
-        var items = superset.OrderBy(keySelectorMethod).Skip(skip).Take(pageSize).ToList();
-
-        Subset.AddRange(items);
     }
 }
 
@@ -112,7 +123,7 @@ public class PagedList<T> : BasePagedList<T>
         {
             var skip = (pageNumber - 1) * pageSize;
 
-            Subset.AddRange(superset.Skip(skip).Take(pageSize));
+            Subset = superset.Skip(skip).Take(pageSize).ToList();
         }
     }
 
