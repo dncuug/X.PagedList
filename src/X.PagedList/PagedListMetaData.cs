e62fa925adecb7b92468ef5cd9d23ec3c56a0a90
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2024-07-13 17:26:41 +0300

    Cleanup xml comments

--- src/X.PagedList/PagedListMetaData.cs
+++ src/X.PagedList/PagedListMetaData.cs
@@ -36,92 +36,58 @@ public class PagedListMetaData : IPagedList
     }
 
     /// <summary>
-    /// 	Total number of subsets within the superset.
+    /// Total number of subsets within the superset.
     /// </summary>
-    /// <value>
-    /// 	Total number of subsets within the superset.
-    /// </value>
     public int PageCount { get; protected set; }
 
     /// <summary>
-    /// 	Total number of objects contained within the superset.
+    /// Total number of objects contained within the superset.
     /// </summary>
-    /// <value>
-    /// 	Total number of objects contained within the superset.
-    /// </value>
     public int TotalItemCount { get; protected set; }
 
     /// <summary>
-    /// 	One-based index of this subset within the superset, zero if the superset is empty.
+    /// One-based index of this subset within the superset, zero if the superset is empty.
     /// </summary>
-    /// <value>
-    /// 	One-based index of this subset within the superset, zero if the superset is empty.
-    /// </value>
     public int PageNumber { get; protected set; }
 
     /// <summary>
-    /// 	Maximum size any individual subset.
+    /// Maximum size any individual subset.
     /// </summary>
-    /// <value>
-    /// 	Maximum size any individual subset.
-    /// </value>
     public int PageSize { get; protected set; }
 
     /// <summary>
-    /// 	Returns true if the superset is not empty and PageNumber is less than or equal to PageCount and this is NOT the first subset within the superset.
+    /// Returns true if the superset is not empty and PageNumber is less than or equal to PageCount and this
+    /// is NOT the first subset within the superset.
     /// </summary>
-    /// <value>
-    /// 	Returns true if the superset is not empty and PageNumber is less than or equal to PageCount and this is NOT the first subset within the superset.
-    /// </value>
     public bool HasPreviousPage { get; protected set; }
 
     /// <summary>
     /// Returns true if the superset is not empty and PageNumber is less than or equal to PageCount and this
     /// is NOT the last subset within the superset.
     /// </summary>
-    /// <value>
-    /// Returns true if the superset is not empty and PageNumber is less than or equal to PageCount and this
-    /// is NOT the last subset within the superset.
-    /// </value>
     public bool HasNextPage { get; protected set; }
 
     /// <summary>
-    /// Returns true if the superset is not empty and PageNumber is less than or equal to PageCount and this
-    /// is the first subset within the superset.
+    /// Returns true if the superset is not empty and PageNumber is less than or equal to PageCount and this  is
+    /// the first subset within the superset.
     /// </summary>
-    /// <value>
-    /// Returns true if the superset is not empty and PageNumber is less than or equal to PageCount and
-    /// this is the first subset within the superset.
-    /// </value>
     public bool IsFirstPage { get; protected set; }
 
     /// <summary>
-    /// Returns true if the superset is not empty and PageNumber is less than or equal to PageCount and
-    /// this is the last subset within the superset.
+    /// Returns true if the superset is not empty and PageNumber is less than or equal to PageCount and this is
+    /// the last subset within the superset.
     /// </summary>
-    /// <value>
-    /// Returns true if the superset is not empty and PageNumber is less than or equal to PageCount and this
-    /// is the last subset within the superset.
-    /// </value>
     public bool IsLastPage { get; protected set; }
 
     /// <summary>
     /// One-based index of the first item in the paged subset, zero if the superset is empty or PageNumber
     /// is greater than PageCount.
     /// </summary>
-    /// <value>
-    /// One-based index of the first item in the paged subset, zero if the superset is empty or PageNumber
-    /// is greater than PageCount.
-    /// </value>
     public int FirstItemOnPage { get; protected set; }
 
     /// <summary>
-    /// One-based index of the last item in the paged subset, zero if the superset is empty or PageNumber
-    /// is greater than PageCount.
+    /// One-based index of the last item in the paged subset, zero if the superset is empty or PageNumber is
+    /// greater than PageCount.
     /// </summary>
-    /// <value>
-    /// One-based index of the last item in the paged subset, zero if the superset is empty or PageNumber
-    /// is greater than PageCount.
-    /// </value>
     public int LastItemOnPage { get; protected set; }
 }
