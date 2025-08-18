159010fd5fe94da50de561de2fb60ed0a8b8248c
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-08-18 09:08:33 +0300

    Add XML documentation to HtmlHelper and TagBuilder extensions for improved clarity

--- src/X.PagedList.Mvc.Core/TagBuilderExtensions.cs
+++ src/X.PagedList.Mvc.Core/TagBuilderExtensions.cs
@@ -6,29 +6,64 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace X.PagedList.Mvc.Core;
 
+/// <summary>
+/// Extension methods for <see cref="TagBuilder"/> to simplify common operations
+/// like adding CSS classes, merging attributes, manipulating inner content,
+/// and rendering the tag to a string with a configurable <see cref="HtmlEncoder"/>.
+/// </summary>
 [PublicAPI]
 public static class TagBuilderExtensions
 {
+    /// <summary>
+    /// Adds one or more CSS classes to the underlying HTML element.
+    /// </summary>
+    /// <param name="tagBuilder">The target <see cref="TagBuilder"/>.</param>
+    /// <param name="value">A space-separated list of CSS class names to add.</param>
     public static void AddCssClass(this TagBuilder tagBuilder, string value)
     {
         tagBuilder.AddCssClass(value);
     }
 
+    /// <summary>
+    /// Appends raw HTML to the element's inner HTML without encoding.
+    /// </summary>
+    /// <param name="tagBuilder">The target <see cref="TagBuilder"/>.</param>
+    /// <param name="innerHtml">The HTML fragment to append.</param>
     public static void AppendHtml(this TagBuilder tagBuilder, string innerHtml)
     {
         tagBuilder.InnerHtml.AppendHtml(innerHtml);
     }
 
+    /// <summary>
+    /// Adds a new attribute or updates an existing one on the element.
+    /// </summary>
+    /// <param name="tagBuilder">The target <see cref="TagBuilder"/>.</param>
+    /// <param name="key">The attribute name.</param>
+    /// <param name="value">The attribute value. May be <c>null</c>.</param>
     public static void MergeAttribute(this TagBuilder tagBuilder, string key, string? value)
     {
         tagBuilder.MergeAttribute(key, value);
     }
 
+    /// <summary>
+    /// Sets the inner text content of the element, HTML-encoding the provided text.
+    /// </summary>
+    /// <param name="tagBuilder">The target <see cref="TagBuilder"/>.</param>
+    /// <param name="innerText">The text to set as the element's content.</param>
     public static void SetInnerText(this TagBuilder tagBuilder, string innerText)
     {
         tagBuilder.InnerHtml.SetContent(innerText);
     }
 
+    /// <summary>
+    /// Renders the <see cref="TagBuilder"/> to a string using the specified render mode and encoder.
+    /// </summary>
+    /// <param name="tagBuilder">The target <see cref="TagBuilder"/>.</param>
+    /// <param name="renderMode">The <see cref="TagRenderMode"/> to use when rendering.</param>
+    /// <param name="encoder">
+    /// Optional <see cref="HtmlEncoder"/> to use during rendering. If not provided, a default encoder is created.
+    /// </param>
+    /// <returns>The rendered HTML string.</returns>
     public static string ToString(this TagBuilder tagBuilder, TagRenderMode renderMode, HtmlEncoder? encoder = null)
     {
         encoder ??= HtmlEncoder.Create(new TextEncoderSettings());
--- src/X.PagedList.Mvc.Core/TagBuilderFactory.cs
+++ src/X.PagedList.Mvc.Core/TagBuilderFactory.cs
@@ -2,12 +2,25 @@
 
 namespace X.PagedList.Mvc.Core;
 
+/// <summary>
+/// Factory interface for creating <see cref="TagBuilder"/> instances.
+/// </summary>
 public interface ITagBuilderFactory
 {
+    /// <summary>
+    /// Creates a new <see cref="TagBuilder"/> for the specified tag name.
+    /// </summary>
+    /// <param name="tagName">The name of the HTML tag to create.</param>
+    /// <returns>A <see cref="TagBuilder"/> instance for the given tag name.</returns>
     TagBuilder Create(string tagName);
 }
 
+/// <summary>
+/// Default implementation of <see cref="ITagBuilderFactory"/>.
+/// Creates <see cref="TagBuilder"/> instances for specified tag names.
+/// </summary>
 internal sealed class TagBuilderFactory : ITagBuilderFactory
 {
+    /// <inheritdoc/>
     public TagBuilder Create(string tagName) => new(tagName);
 }
